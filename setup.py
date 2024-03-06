301cd3e3e769f817230f2131a2a28502502ab4ef
MartinWie <donotsuspend@googlegroups.com>
2024-03-06 01:40:43 +0100

    Had a dependency issue on multiple machines. Changed version to 0.6.0 and published a fresh pip package as a quick pragmatic cleanup. All affected machines could successfully update afterward

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='qprom',
-      version='0.5.8',
+      version='0.6.0',
       description='A Python-based CLI tool to quickly interact with OpenAIs GPT models instead of relying on the web interface.',
       long_description=readme(),
       long_description_content_type='text/markdown',
